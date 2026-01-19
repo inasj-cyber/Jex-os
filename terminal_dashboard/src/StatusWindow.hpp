6a07094800c232a7caf14166545b643c0343755b
JexOS Dev <you@jexos.org>
2026-01-19 18:46:58 +0100

    Remove unrelated C++ project folders from JexOS

--- terminal_dashboard/src/StatusWindow.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Window.hpp"
-
-class StatusWindow : public Window {
-public:
-    StatusWindow(int x, int y, int width, int height, const std::string& title);
-    
-    void draw() const override;
-};
